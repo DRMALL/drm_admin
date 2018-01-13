1c9840e2e55b2d8c2cd50857efc439d57733d600
joephon <306519779@qq.com>
2018-01-13 12:30:43 +0800

    added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var path = require('path');
+var PORT = 3008
 
 
 app.use(express.static('./build'));
@@ -11,6 +12,10 @@ app.use(cors());
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, './build', 'index.html'))
 })
+
+app.listen(PORT, function () {
+  console.log('Node app is running, port:', PORT);
+});
 
 
 //module.exports =app
